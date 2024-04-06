9bdcaf4a775b223516e95c6c5e5e3e6373a75a37
Adwaith Kallungal Vrundavanan <ak23abn@herts.ac.uk>
2024-04-06 19:11:02 +0100

    Added functionality to convert O3DPC to ROS2PC2

--- converter.py
+++ converter.py
@@ -38,3 +38,52 @@ def ROSpc2_to_O3DPointCloud(pc2):
     open3d_cloud.points = o3d.utility.Vector3dVector(np.array(xyz)) #numpy to o3d
 
     return open3d_cloud
+
+# Reference: https://github.com/SebastianGrans/ROS2-Point-Cloud-Demo/blob/master/pcd_demo/pcd_publisher/pcd_publisher_node.py
+def O3DPointCloud_to_ROSpc2(open3d_cloud, frame_id="map"):
+    """ Creates a point cloud message.
+    Args:
+        points: Nx3 array of xyz positions.
+        parent_frame: frame in which the point cloud is defined
+    Returns:
+        sensor_msgs/PointCloud2 message
+
+    Code source:
+        https://gist.github.com/pgorczak/5c717baa44479fa064eb8d33ea4587e0
+
+    References:
+        http://docs.ros.org/melodic/api/sensor_msgs/html/msg/PointCloud2.html
+        http://docs.ros.org/melodic/api/sensor_msgs/html/msg/PointField.html
+        http://docs.ros.org/melodic/api/std_msgs/html/msg/Header.html
+
+    """
+    # In a PointCloud2 message, the point cloud is stored as an byte
+    # array. In order to unpack it, we also include some parameters
+    # which desribes the size of each individual point.
+    ros_dtype = PointField.FLOAT32
+    dtype = np.float32
+    itemsize = np.dtype(dtype).itemsize  # A 32-bit float takes 4 bytes.
+    np_open3d_cloud = np.asarray(open3d_cloud.points)
+    data = np_open3d_cloud.astype(dtype).tobytes()
+
+    # The fields specify what the bytes represents. The first 4 bytes
+    # represents the x-coordinate, the next 4 the y-coordinate, etc.
+    fields = [PointField(
+        name=n, offset=i * itemsize, datatype=ros_dtype, count=1)
+        for i, n in enumerate('xyz')]
+
+    # The PointCloud2 message also has a header which specifies which
+    # coordinate frame it is represented in.
+    header = Header(frame_id=frame_id)
+
+    return PointCloud2(
+        header=header,
+        height=1,
+        width=np_open3d_cloud.shape[0],
+        is_dense=False,
+        is_bigendian=False,
+        fields=fields,
+        point_step=(itemsize * 3),  # Every point consists of three float32s.
+        row_step=(itemsize * 3 * np_open3d_cloud.shape[0]),
+        data=data
+    )
